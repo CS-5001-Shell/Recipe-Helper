80e5e535aaea81ac53ab45c4e40e1968325ce01e
Sami Rollins <srollins@gmail.com>
2024-09-11 14:23:39 -0700

    update return type hint

--- recipe_helper.py
+++ recipe_helper.py
@@ -2,14 +2,16 @@
 def get_amount_needed_in_us_std(
         us_std_unit: str, 
         metric_amount_needed: float, 
-        metric_unit: str):
+        metric_unit: str) -> float:
       """
       Given an amount needed in metric units, determines
       the amount needed in US standard units.
       Parameters:
-      us_std_units(str): US standard units, e.g., tablespoons
-      metric_amount_needed(float): Amount required in metric, e.g., 725
-      metric_units(str): Metric units, e.g., ml
+        us_std_units(str): US standard units, e.g., tablespoons
+        metric_amount_needed(float): Amount required in metric, e.g., 725
+        metric_units(str): Metric units, e.g., ml
+      Returns:
+        amount in US standard (float)
       """
       pass
 
